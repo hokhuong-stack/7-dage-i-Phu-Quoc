d56ca1a5004a7971f0a1ab1e1d79afd83289a823
hokhuong-stack <ho.khuong@gmail.com>
2026-09-05 17:43:41 +0200

    Refactor music controls and add day display functionality
    Refactor toggleMusik function to simplify play logic and improve error handling. Add visDag function to manage day display and autoplay music.

--- script.js
+++ script.js
@@ -6,24 +6,65 @@ function toggleMusik() {
     let knap = document.getElementById("musik-knap");
 
     if (lyd.paused) {
-        // Tving mobilen til at indlæse lyden med det samme
-        lyd.load();
-        
-        let afspilningsForsog = lyd.play();
-        if (afspilningsForsog !== undefined) {
-            afspilningsForsog.then(() => {
-                knap.innerHTML = "🔇 Sluk musik";
-                knap.classList.add("spiller");
-                musikStartet = true;
-                brugerHarKlikketPause = false;
-            }).catch(error => {
-                console.log("Mobilen blokerede afspilning: ", error);
-            });
-        }
+        lyd.play().then(() => {
+            knap.innerHTML = "🔇 Sluk musik";
+            knap.classList.add("spiller");
+            musikStartet = true;
+            brugerHarKlikketPause = false;
+        }).catch(error => {
+            console.log("Fejl ved afspilning: ", error);
+        });
     } else {
         lyd.pause();
         knap.innerHTML = "🎵 Tænd musik";
         knap.classList.remove("spiller");
         brugerHarKlikketPause = true;
     }
+}
+
+function visDag(event, dagId) {
+    let indhold = document.getElementsByClassName("program-indhold");
+    for (let i = 0; i < indhold.length; i++) {
+        indhold[i].style.display = "none";
+    }
+
+    let knapper = document.getElementsByClassName("fane-knap");
+    for (let i = 0; i < knapper.length; i++) {
+        knapper[i].className = knapper[i].className.replace(" aktiv", "");
+    }
+
+    let markorer = document.getElementsByClassName("kort-marker");
+    for (let i = 0; i < markorer.length; i++) {
+        markorer[i].classList.remove("lyser-op");
+        markorer[i].style.color = ""; 
+    }
+
+    document.getElementById(dagId).style.display = "block";
+    event.currentTarget.className += " aktiv";
+
+    let valgtMarker = document.getElementById("marker-" + dagId);
+    if (valgtMarker) {
+        valgtMarker.classList.add("lyser-op");
+        valgtMarker.style.color = "white";
+    }
+
+    if (musikStartet === false && brugerHarKlikketPause === false) {
+        let lyd = document.getElementById("ferie-musik");
+        let knap = document.getElementById("musik-knap");
+        if (lyd) {
+            lyd.play().then(() => {
+                musikStartet = true;
+                knap.innerHTML = "🔇 Sluk musik";
+                knap.classList.add("spiller");
+            }).catch(e => {
+                console.log("Autoplay blokeret", e);
+            });
+        }
+    }
+
+    // Ruller skærmen blødt ned til toppen af programmet
+    document.querySelector('.indhold-wrapper').scrollIntoView({ 
+        behavior: 'smooth', 
+        block: 'start' 
+    });
 }
